b3e0aea62c55f0b745b6ae514c6e846a5fdd404b
Sylvain FLINOIS <sflinois@e2r6p6.42.fr>
2017-03-25 13:59:06 +0100

    rm onject, lib, main ans sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-cp main.c main2.c
-sed -i '' 's/printf/ft_printf/g' main2.c
-gcc -Wall -Wextra -Werror -o printf libftprintf.a main.c
-gcc -Wall -Wextra -Werror -o ft_printf libftprintf.a main2.c
-./printf > printf.txt
-./ft_printf > ft_printf.txt
-cmp printf.txt ft_printf.txt > cmp.txt
-rm printf.txt ft_printf.txt main2.c
-cat cmp.txt
